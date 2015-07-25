712339e7c319ef0abe69e6297d6e398f299f2d65
warfine <yakhin.v@gmail.com>
2015-07-25 17:41:40 +0300

    added postcss and autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp'),
-    minifyCss = require('gulp-minify-css'),
-    minifyHTML = require('gulp-minify-html');
+  minifyCss = require('gulp-minify-css'),
+  minifyHTML = require('gulp-minify-html'),
+  postcss = require('gulp-postcss'),
+  autoprefixer = require('autoprefixer-core');
 
 gulp.task('default', function() {
   // place code for your default task here
@@ -16,4 +18,15 @@ gulp.task('minify-html', function() {
   return gulp.src('src/*.html')
     .pipe(minifyHTML())
     .pipe(gulp.dest('dist'));
+});
+
+gulp.task('css', function() {
+  var processors = [
+    autoprefixer({
+      browsers: ['last 1 version']
+    })
+  ];
+  return gulp.src('src/css/*.css')
+    .pipe(postcss(processors))
+    .pipe(gulp.dest('dest/css'));
 });
