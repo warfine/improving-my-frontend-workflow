b7813e239c46174231c2e014baecad33a3fbe643
warfine <yakhin.v@gmail.com>
2015-07-25 17:02:36 +0300

    added and tested npm plugin minify-html

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'),
-    minifyCss = require('gulp-minify-css');
+    minifyCss = require('gulp-minify-css'),
+    minifyHTML = require('gulp-minify-html');
 
 gulp.task('default', function() {
   // place code for your default task here
@@ -9,4 +10,10 @@ gulp.task('minify-css', function() {
   return gulp.src('src/css/*.css')
     .pipe(minifyCss())
     .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('minify-html', function() {
+  return gulp.src('src/*.html')
+    .pipe(minifyHTML())
+    .pipe(gulp.dest('dist'));
 });
