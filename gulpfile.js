7fefb9fef37cc106a26363ce1c8270a4caf0d215
warfine <yakhin.v@gmail.com>
2015-07-25 16:07:03 +0300

    added and tested plugin minify-css

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    minifyCss = require('gulp-minify-css');
 
 gulp.task('default', function() {
   // place code for your default task here
+});
+
+gulp.task('minify-css', function() {
+  return gulp.src('src/css/*.css')
+    .pipe(minifyCss())
+    .pipe(gulp.dest('dist/css'));
 });
